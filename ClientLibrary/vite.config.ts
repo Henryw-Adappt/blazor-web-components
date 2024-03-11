104c5a7de0237587edcb7d209f182ec5f80b3c6b
Henry Winchester <130577603+Henryw-Adappt@users.noreply.github.com>
2024-03-11 19:24:43 +0000

    Vite use index for dev server

--- ClientLibrary/vite.config.ts
+++ ClientLibrary/vite.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: 'src',
-    assetsInclude: ['assets/**/*'],
-    build: {
-        lib: {
-            entry: 'lib/public-api.ts',
-            name: 'ClientLibrary',
-            fileName: (format, filename) => {
-                if (format === 'umd') return 'client-library.js';
-                else return 'cl.js';
-            },
-            formats: ['umd'],
-
-        },
-        outDir: '../wwwroot'
-    }
-})
+  root: 'src',
+  build: {
+    lib: {
+      entry: 'lib/public-api.ts',
+      name: 'ClientLibrary',
+      fileName: (format, filename) => {
+        if (format === 'umd') return 'client-library.js';
+        else return 'cl.js';
+      },
+      formats: ['umd'],
+    },
+    outDir: '../wwwroot',
+  },
+});

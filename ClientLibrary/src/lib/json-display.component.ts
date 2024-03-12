82620bdf3d5ee9f2602143edbd961c041981add1
Henry Winchester <130577603+Henryw-Adappt@users.noreply.github.com>
2024-03-12 09:24:34 +0000

    C# to TS type support

--- ClientLibrary/src/lib/json-display.component.ts
+++ ClientLibrary/src/lib/json-display.component.ts
@@ -1,13 +1,21 @@
 import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { AppState, IAppState } from "../server/types";
 
 @customElement('app-json-display')
 export class JSONDisplayComponent extends LitElement {
   @property({ type: Object })
-  public obj: object = {};
+  public set state(state: IAppState) {
+    this._state = AppState.fromJS(state);
+  }
+  public get state() {
+    return this._state;
+  }
+  private _state!: AppState;
 
   public render() {
-    return html`<code>${JSON.stringify(this.obj)}</code>`;
+    console.log(this._state);
+    return html`<p>${this._state.appName}</p>`;
   }
 }
 
---
+++ ClientLibrary/src/server/types.ts
@@ -0,0 +1,1 @@
+export * from "../../../CommonLibrary/wwwroot/types";
